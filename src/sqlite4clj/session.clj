8d9de3e4e3c23b24267436b2488a78eb51f010fc
andersmurphy <andersmurphy@gmail.com>
2026-01-14 16:45:15 +0000

    Add function to view session changeset

--- src/sqlite4clj/session.clj
+++ src/sqlite4clj/session.clj
@@ -72,6 +72,18 @@
    ::mem/pointer ;; First arg to conflict
    ] ::mem/int)
 
+(defcfn session-create
+  "sqlite3session_create"
+  [::mem/pointer ::mem/c-string ::mem/pointer] ::mem/int
+  sqlite3session-create-native
+  [pdb]
+  (with-open [arena (mem/confined-arena)]
+    (let [ppSession (mem/alloc-instance ::mem/pointer arena)
+          code      (sqlite3session-create-native pdb "main" ppSession)]
+      (if (api/sqlite-ok? code)
+        (mem/deserialize-from ppSession ::mem/pointer)
+        (throw (api/sqlite-ex-info pdb code {}))))))
+
 (defn new-session
   "Creates a session and attaches it to the database."
   [conn]
@@ -111,6 +123,80 @@
         (api/free pInvertSet)
         (reset! session nil)))))
 
+(defcfn changeset-start
+  "sqlite3changeset_start"
+  [::mem/pointer ;; changeset iterator
+   ::mem/int     ;; size of changeset
+   ::mem/pointer ;; changeset
+   ] ::mem/int
+  sqlite3session-changeset-start-native
+  [pdb nSet pSet]
+  (with-open [arena (mem/confined-arena)]
+    (let [ppChangesetIter (mem/alloc-instance ::mem/pointer arena)
+          code            (sqlite3session-changeset-start-native
+                            ppChangesetIter nSet pSet)]
+      (if (api/sqlite-ok? code)
+        (mem/deserialize-from ppChangesetIter ::mem/pointer)
+        (throw (api/sqlite-ex-info pdb code {}))))))
+
+(defcfn changeset-next
+  sqlite3changeset_next [::mem/pointer] ::mem/int)
+
+(def op->statemen {18 "INSERT" 9  "DELETE" 23 "UPDATE"})
+
+(defcfn changeset-op
+  "sqlite3changeset_op"
+  [::mem/pointer ;; IN: changeset iterator
+   ::mem/pointer ;; OUT: table name
+   ::mem/pointer ;; OUT: number of columns in table
+   ::mem/pointer ;; OUT: statement
+   ::mem/pointer ;; OUT: indirect change
+   ] ::mem/int
+  sqlite3changeset-op-native
+  [pdb pChangesetIter]
+  (with-open [arena (mem/confined-arena)]
+    (let [pzTab      (mem/alloc-instance ::mem/pointer arena)
+          pnCol      (mem/alloc-instance ::mem/pointer arena)
+          pOp        (mem/alloc-instance ::mem/pointer arena)
+          pbIndirect (mem/alloc-instance ::mem/pointer arena)
+
+          code (sqlite3changeset-op-native
+                 pChangesetIter pzTab pnCol pOp pbIndirect)]
+      (if (api/sqlite-ok? code)
+        [(mem/deserialize-from pzTab      ::mem/c-string)
+         (mem/deserialize-from pnCol      ::mem/int)
+         (-> (mem/deserialize-from pOp        ::mem/int)
+             op->statemen)
+         (if (= (mem/deserialize-from pbIndirect ::mem/int) 0)
+           false true)]
+        (throw (api/sqlite-ex-info pdb code {}))))))
+
+(defcfn changeset-finalize
+  sqlite3changeset_finalize [::mem/pointer] ::mem/int)
+
+(defn view-session-changeset
+  "Returns changeset data from session as edn."
+  [conn session]
+  (d/with-conn [conn conn]
+    (when-let [pSession @session]
+      (let [pdb         (:pdb conn)
+            [nSet pSet] (session-changeset pdb pSession)
+            pIter       (changeset-start pdb nSet pSet)
+            ret         (loop [ret (transient [])]
+                          (let [code (int
+                                       #_{:clj-kondo/ignore [:type-mismatch]}
+                                       (changeset-next pIter))]
+                            (case code
+                              ;; TODO:
+                              100 (recur (conj! ret
+                                           (changeset-op pdb pIter)))
+                              101 (persistent! ret)
+                              (throw (api/sqlite-ex-info
+                                       conn code {})))))]
+        (changeset-finalize pIter)
+        (api/free pSet)
+        ret))))
+
 (comment
 
   (defonce db
@@ -119,10 +205,7 @@
        :pool-size 4
        :pragma    {:foreign_keys false}}))
 
-  (def reader  (db :reader))
-  (def writer (db :writer))
-
-  (d/q writer
+  (d/q (db :writer)
     ["CREATE TABLE IF NOT EXISTS bar(id INT PRIMARY KEY, data BLOB)"])
 
   (let [session (d/with-conn [conn (:writer db)]
@@ -132,15 +215,13 @@
       ["insert into bar (id, data) VALUES (?, ?)"
        (str (random-uuid)) 34])
     (println (d/q (:reader db) ["select count(*) from bar"]))
-    (d/with-conn [conn (:writer db)]
+    (d/with-conn [conn (:writer db)]      
+    (clojure.pprint/pprint (view-session-changeset conn session))
       (undo-session conn session))
     (println (d/q (:reader db) ["select count(*) from bar"])))
-
   
   (let [old-sesion (new-session (:writer db))]
     (d/with-write-tx [conn (:writer db)]
       (undo-session conn old-sesion)
       (new-session conn)))
-
-  (+ 3 4)
   )
